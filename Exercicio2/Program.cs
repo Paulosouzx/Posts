3395ab50c4a4cb1f35b37b48a8411e47e0b2c768
Paulo Souza <paulosouzx14@gmail.com>
2024-03-19 11:41:37 +0000

    Add project files.

---
+++ Exercicio2/Program.cs
@@ -0,0 +1,37 @@
+﻿using Exercicio2.Entities;
+using System;
+
+namespace Exercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1 = new Comment("Have a nice trip");
+            Comment c2 = new Comment("Wow that's awesome!");
+            Post p1 = new Post(
+        DateTime.ParseExact("19/03/2024 11:18", "dd/MM/yyyy HH:mm", null),
+            "Traveling to New Zealand",
+            "I'm going to visit this wonderful country!", 12);
+
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Comment c3 = new Comment("Good night");
+            Comment c4 = new Comment("May the Force be with you");
+            Post p2 = new Post(
+        DateTime.ParseExact("19/03/2024 11:18", "dd/MM/yyyy HH:mm", null),
+                    "Good night guys",
+                    "See you tomorrow",
+                    5);
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+
+            Console.ReadKey();
+        }
+        
+    }
+}
